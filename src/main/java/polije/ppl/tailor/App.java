0249cb6901c75815235c937808bea3751b80b771
irsyadulibad <ahmadirsyadulibad7@gmail.com>
2022-11-22 15:50:08 +0700

    init: create project

---
+++ src/main/java/polije/ppl/tailor/App.java
@@ -0,0 +1,13 @@
+package polije.ppl.tailor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
