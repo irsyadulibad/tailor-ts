959afcfe107adb984d45385147717c6b1d5143b9
irsyadulibad <ahmadirsyadulibad7@gmail.com>
2022-12-16 10:48:49 +0700

    chore(transaction): handle nullable due date

--- src/main/java/polije/ppl/tailor/repository/TransactionRepository.java
+++ src/main/java/polije/ppl/tailor/repository/TransactionRepository.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.Types;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -20,51 +21,58 @@ public class TransactionRepository implements Repository<Transaction> {
         String sql = "SELECT * FROM " + tableName;
         List<Transaction> transactions = new ArrayList<>();
 
-        try(PreparedStatement stmt = conn.prepareStatement(sql)) {
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
             ResultSet results = stmt.executeQuery();
 
-            while(results.next()) {
+            while (results.next()) {
                 transactions.add(mapToEntity(results));
             }
-        } catch (SQLException e) {}
+        } catch (SQLException e) {
+        }
 
         return transactions;
     }
 
     public Transaction get(Integer id) {
-        String sql = "SELECT * FROM "+ tableName +" WHERE transaction_id = ?";
+        String sql = "SELECT * FROM " + tableName + " WHERE transaction_id = ?";
         Transaction transaction = new Transaction();
 
-        try(PreparedStatement stmt = conn.prepareStatement(sql);) {
+        try (PreparedStatement stmt = conn.prepareStatement(sql);) {
             stmt.setInt(1, id);
             ResultSet rs = stmt.executeQuery();
 
-            if(rs.next()) { return mapToEntity(rs); }
-        } catch(SQLException e) {}
+            if (rs.next()) {
+                return mapToEntity(rs);
+            }
+        } catch (SQLException e) {
+        }
 
         return transaction;
     }
 
     public List<Transaction> get(Map<String, Object> values) {
         int iterate = 0;
-        String sql = "SELECT * FROM "+ tableName +" WHERE ";
+        String sql = "SELECT * FROM " + tableName + " WHERE ";
         List<Transaction> transactions = new ArrayList<>();
 
-        for(String valueKey: values.keySet()) {
-            if(iterate > 0) sql += " AND ";
-            sql += valueKey +" = ?";
+        for (String valueKey : values.keySet()) {
+            if (iterate > 0)
+                sql += " AND ";
+            sql += valueKey + " = ?";
 
             iterate++;
         }
 
-        try(PreparedStatement stmt = conn.prepareStatement(sql)) {
-            DatabaseUtil.prepareStmt(stmt, values);;
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+            DatabaseUtil.prepareStmt(stmt, values);
+            ;
             ResultSet rs = stmt.executeQuery();
 
-            while(rs.next()) {
+            while (rs.next()) {
                 transactions.add(mapToEntity(rs));
             }
-        }catch(SQLException e) {}
+        } catch (SQLException e) {
+        }
 
         return transactions;
     }
@@ -75,11 +83,17 @@ public class TransactionRepository implements Repository<Transaction> {
         try(PreparedStatement stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
             stmt.setString(1, trans.getStatus().toString());
             stmt.setDate(2, new Date(trans.getDate().getTime()));
-            stmt.setDate(3, new Date(trans.getDueDate().getTime()));
             stmt.setInt(4, trans.getTotal());
             stmt.setString(5, trans.getNote());
             stmt.setInt(6, trans.getAccount().getId());
             stmt.setInt(7, trans.getCustomer().getId());
+
+            if (trans.getDueDate() == null) {
+                stmt.setNull(3, Types.DATE);
+            } else {
+                stmt.setDate(3, new Date(trans.getDueDate().getTime()));
+            }
+
             stmt.executeUpdate();
 
             ResultSet rs = stmt.getGeneratedKeys();
@@ -90,9 +104,10 @@ public class TransactionRepository implements Repository<Transaction> {
     }
 
     public boolean update(Transaction trans) {
-        String sql = "UPDATE "+ tableName +" SET status = ?, due_date = ?, total = ?, note = ? WHERE transaction_id = ?";
+        String sql = "UPDATE " + tableName
+                + " SET status = ?, due_date = ?, total = ?, note = ? WHERE transaction_id = ?";
 
-        try(PreparedStatement stmt = conn.prepareStatement(sql)) {
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
             stmt.setString(1, trans.getStatus().toString());
             stmt.setDate(2, new Date(trans.getDueDate().getTime()));
             stmt.setInt(3, trans.getTotal());
@@ -101,20 +116,24 @@ public class TransactionRepository implements Repository<Transaction> {
 
             stmt.executeUpdate();
             return stmt.getUpdateCount() > 0;
-        } catch(SQLException e) { e.printStackTrace(); }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
         return false;
     }
 
     public boolean delete(int id) {
-        String sql = "DELETE FROM "+ tableName +" WHERE transaction_id = ?";
+        String sql = "DELETE FROM " + tableName + " WHERE transaction_id = ?";
 
-        try(PreparedStatement stmt = conn.prepareStatement(sql)) {
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
             stmt.setInt(1, id);
             stmt.executeUpdate();
 
             return stmt.getUpdateCount() > 0;
-        } catch(SQLException e) { e.printStackTrace(); }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
         return false;
     }
@@ -124,14 +143,13 @@ public class TransactionRepository implements Repository<Transaction> {
         int accId = result.getInt("account_id");
 
         Transaction transaction = new Transaction(
-            result.getInt("total"),
-            new AccountRepository().get(accId),
-            new CustomerRepository().get(custId),
-            result.getDate("date"),
-            result.getDate("due_date"),
-            result.getString("note"),
-            TransactionStatus.valueOf(result.getString("status"))
-        );
+                result.getInt("total"),
+                new AccountRepository().get(accId),
+                new CustomerRepository().get(custId),
+                result.getDate("date"),
+                result.getDate("due_date"),
+                result.getString("note"),
+                TransactionStatus.valueOf(result.getString("status")));
 
         transaction.setId(result.getInt("transaction_id"));
         return transaction;
