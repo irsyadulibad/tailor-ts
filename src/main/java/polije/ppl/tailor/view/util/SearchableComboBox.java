765d3ffd9f5c71aac7e31005962da2a23c0536d6
irsyadulibad <ahmadirsyadulibad7@gmail.com>
2022-12-20 06:16:10 +0700

    chore: refactor code

--- src/main/java/polije/ppl/tailor/view/util/SearchableComboBox.java
+++ src/main/java/polije/ppl/tailor/view/util/SearchableComboBox.java
@@ -20,39 +20,39 @@ import polije.ppl.tailor.data.ComboItem;
 public class SearchableComboBox extends JComboBox {
     public int caretPos = 0;
     public JTextField tField = null;
-    
+
     public SearchableComboBox(final ComboItem items[]) {
         super(items);
-        
+
         setEditor(new BasicComboBoxEditor());
         setEditable(true);
     }
-    
+
     public void setSelectedIndex(int index) {
         super.setSelectedIndex(index);
-        
+
         tField.setText(getItemAt(index).toString());
         tField.setSelectionEnd(caretPos + tField.getText().length());
         tField.moveCaretPosition(caretPos);
     }
-    
+
     public void setEditor(ComboBoxEditor editor) {
         super.setEditor(editor);
-        
+
         if(editor.getEditorComponent() instanceof JTextField) {
             tField = (JTextField) editor.getEditorComponent();
             tField.addKeyListener(new KeyAdapter() {
                 public void keyReleased(KeyEvent ke) {
                     char key = ke.getKeyChar();
                     if(!Character.isLetterOrDigit(key) || Character.isSpaceChar(key)) return;
-                    
+
                     caretPos = tField.getCaretPosition();
                     String text = "";
-                    
+
                     try {
                         text = tField.getText(0, caretPos);
                     } catch(BadLocationException e) { e.printStackTrace(); }
-                    
+
                     for(int i = 0; i < getItemCount(); i++) {
                         ComboItem element = (ComboItem) getItemAt(i);
                         if(element.getVal().startsWith(text)) {
