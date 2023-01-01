3f86a3cdf2933d51defbefc7407e3cbc56ba7061
irsyadulibad <ahmadirsyadulibad7@gmail.com>
2023-01-01 18:13:22 +0700

    set nimbus look and feel

--- src/main/java/polije/ppl/tailor/App.java
+++ src/main/java/polije/ppl/tailor/App.java
@@ -1,5 +1,7 @@
 package polije.ppl.tailor;
 
+import javax.swing.UIManager.LookAndFeelInfo;
+
 import polije.ppl.tailor.view.LoadingPageView;
 
 /**
@@ -10,6 +12,19 @@ public class App
 {
     public static void main( String[] args )
     {
-        LoadingPageView.main(args);
+        try {
+            for(LookAndFeelInfo info: javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if(info.getName().equals("Nimbus")) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch(Exception e) { e.printStackTrace(); }
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                LoadingPageView.main(args);
+            }
+        });
     }
 }
