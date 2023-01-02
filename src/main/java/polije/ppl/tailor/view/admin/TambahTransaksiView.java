89f8723e07ed2da844a973be7bc27d62f388a799
irsyadulibad <ahmadirsyadulibad7@gmail.com>
2023-01-02 21:16:51 +0700

    fix(transaction): reset button

--- src/main/java/polije/ppl/tailor/view/admin/TambahTransaksiView.java
+++ src/main/java/polije/ppl/tailor/view/admin/TambahTransaksiView.java
@@ -439,7 +439,11 @@ public class TambahTransaksiView extends javax.swing.JFrame {
     }//GEN-LAST:event_btn_saveMouseClicked
 
     private void btn_resetMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btn_resetMouseClicked
+        namapakaian.setText("");
+        catatan.setText("");
+        details.clear();
 
+        loadTable();
     }//GEN-LAST:event_btn_resetMouseClicked
 
     private void hargaKeyReleased(java.awt.event.KeyEvent evt) {
